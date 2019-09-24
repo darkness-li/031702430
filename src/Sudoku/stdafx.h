16f521053d199d4ad0b4aba053a171cc24cf1b06
darkness-li <54981077+darkness-li@users.noreply.github.com>
2019-09-24 12:33:39 +0800

    Create stdafx.h

---
+++ src/Sudoku/stdafx.h
@@ -0,0 +1,8 @@
+#pragma once
+int check1(int x, int y, int m, int num);
+int check2(int x, int y, int m, int num);
+int check3(int x, int y, int m, int num);
+int dfs(int n, int m);
+void sudok1(int m, int count);
+void sudok2(int m, int count);
+void sudok3(int m, int count);
