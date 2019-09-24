de030378669f63b21f6a761e68258a3988fb67e6
darkness-li <54981077+darkness-li@users.noreply.github.com>
2019-09-24 13:31:51 +0800

    Update stdafx.h

--- src/Sudoku/stdafx.h
+++ src/Sudoku/stdafx.h
@@ -1,8 +1,7 @@
 #pragma once
-int check1(int x, int y, int m, int num);
-int check2(int x, int y, int m, int num);
-int check3(int x, int y, int m, int num);
-int dfs(int n, int m);
-void sudok1(int m, int count);
-void sudok2(int m, int count);
-void sudok3(int m, int count);
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<fstream>
+#include<cmath>
+#pragma warning(disable:4996)
